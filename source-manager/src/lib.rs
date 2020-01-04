0a87ec3f18a0eaead1af97e2f5036e74558f259f
Noam Raz <noamraz8@gmail.com>
2020-01-05 00:28:57 +0200

    Use 4 spaces for indentation

--- source-manager/src/lib.rs
+++ source-manager/src/lib.rs
@@ -8,138 +8,138 @@ mod source_pos;
 pub use source_pos::*;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
-pub struct SourceId(u32);
+pub struct SourceId(usize);
 
 impl SourceId {
-  pub fn from_raw(raw: u32) -> Self {
-    SourceId(raw)
-  }
+    pub fn from_raw(raw: usize) -> Self {
+        SourceId(raw)
+    }
 
-  pub fn to_raw(&self) -> u32 {
-    self.0
-  }
+    pub fn to_raw(&self) -> usize {
+        self.0
+    }
 }
 
 struct FileSourceInfo {
-  filename: Box<Path>,
-  src: String,
-  include_pos: Option<SourcePos>,
+    filename: Box<Path>,
+    src: String,
+    include_pos: Option<SourcePos>,
 }
 
 impl FileSourceInfo {
-  pub fn new(filename: impl AsRef<Path>, src: String, include_pos: Option<SourcePos>) -> Self {
-    FileSourceInfo {
-      filename: PathBuf::from(filename.as_ref()).into_boxed_path(),
-      src,
-      include_pos,
+    pub fn new(filename: impl AsRef<Path>, src: String, include_pos: Option<SourcePos>) -> Self {
+        FileSourceInfo {
+            filename: PathBuf::from(filename.as_ref()).into_boxed_path(),
+            src,
+            include_pos,
+        }
     }
-  }
 
-  pub fn src(&self) -> &str {
-    &self.src
-  }
+    pub fn src(&self) -> &str {
+        &self.src
+    }
 
-  pub fn filename(&self) -> &Path {
-    &self.filename
-  }
+    pub fn filename(&self) -> &Path {
+        &self.filename
+    }
 
-  pub fn include_pos(&self) -> Option<SourcePos> {
-    self.include_pos
-  }
+    pub fn include_pos(&self) -> Option<SourcePos> {
+        self.include_pos
+    }
 }
 
 struct ExpansionSourceInfo {
-  spelling_pos: SourcePos,
-  expansion_range: SourceRange,
+    spelling_pos: SourcePos,
+    expansion_range: SourceRange,
 }
 
 impl ExpansionSourceInfo {
-  pub fn new(spelling_pos: SourcePos, expansion_range: SourceRange) -> Self {
-    ExpansionSourceInfo {
-      spelling_pos,
-      expansion_range,
+    pub fn new(spelling_pos: SourcePos, expansion_range: SourceRange) -> Self {
+        ExpansionSourceInfo {
+            spelling_pos,
+            expansion_range,
+        }
     }
-  }
 
-  pub fn spelling_pos(&self) -> SourcePos {
-    self.spelling_pos
-  }
+    pub fn spelling_pos(&self) -> SourcePos {
+        self.spelling_pos
+    }
 
-  pub fn expansion_range(&self) -> SourceRange {
-    self.expansion_range
-  }
+    pub fn expansion_range(&self) -> SourceRange {
+        self.expansion_range
+    }
 }
 
 enum SourceInfo {
-  File(FileSourceInfo),
-  Expansion(ExpansionSourceInfo),
+    File(FileSourceInfo),
+    Expansion(ExpansionSourceInfo),
 }
 
 struct Source {
-  pub offset: u32,
-  pub info: SourceInfo,
+    pub offset: u32,
+    pub info: SourceInfo,
 }
 
 pub struct SourcesTooLargeError;
 
 pub struct SourceManager {
-  sources: Vec<Source>,
-  next_offset: u32,
+    sources: Vec<Source>,
+    next_offset: u32,
 }
 
 impl SourceManager {
-  pub fn new() -> Self {
-    SourceManager {
-      sources: vec![],
-      next_offset: 0,
+    pub fn new() -> Self {
+        SourceManager {
+            sources: vec![],
+            next_offset: 0,
+        }
     }
-  }
 
-  fn add_source(
-    &mut self,
-    ctor: impl FnOnce() -> SourceInfo,
-    len: u32,
-  ) -> Result<SourceId, SourcesTooLargeError> {
-    let offset = self.next_offset;
-    self.next_offset = match self.next_offset.checked_add(len) {
-      Some(off) => off,
-      None => return Err(SourcesTooLargeError),
-    };
+    fn add_source(
+        &mut self,
+        ctor: impl FnOnce() -> SourceInfo,
+        len: u32,
+    ) -> Result<SourceId, SourcesTooLargeError> {
+        let offset = self.next_offset;
+        self.next_offset = match self.next_offset.checked_add(len) {
+            Some(off) => off,
+            None => return Err(SourcesTooLargeError),
+        };
 
-    self.sources.push(Source {
-      offset,
-      info: ctor(),
-    });
+        self.sources.push(Source {
+            offset,
+            info: ctor(),
+        });
 
-    Ok(SourceId::from_raw(self.sources.len() as u32))
-  }
+        Ok(SourceId::from_raw(self.sources.len()))
+    }
 
-  pub fn create_file(
-    &mut self,
-    filename: impl AsRef<Path>,
-    src: String,
-    include_pos: Option<SourcePos>,
-  ) -> Result<SourceId, SourcesTooLargeError> {
-    let len = match src.len().try_into() {
-      Ok(len) => len,
-      Err(..) => return Err(SourcesTooLargeError),
-    };
+    pub fn create_file(
+        &mut self,
+        filename: impl AsRef<Path>,
+        src: String,
+        include_pos: Option<SourcePos>,
+    ) -> Result<SourceId, SourcesTooLargeError> {
+        let len = match src.len().try_into() {
+            Ok(len) => len,
+            Err(..) => return Err(SourcesTooLargeError),
+        };
 
-    self.add_source(
-      || SourceInfo::File(FileSourceInfo::new(filename, src, include_pos)),
-      len,
-    )
-  }
+        self.add_source(
+            || SourceInfo::File(FileSourceInfo::new(filename, src, include_pos)),
+            len,
+        )
+    }
 
-  pub fn create_expansion(
-    &mut self,
-    spelling_pos: SourcePos,
-    expansion_range: SourceRange,
-    tok_len: u32,
-  ) -> Result<SourceId, SourcesTooLargeError> {
-    self.add_source(
-      || SourceInfo::Expansion(ExpansionSourceInfo::new(spelling_pos, expansion_range)),
-      tok_len,
-    )
-  }
+    pub fn create_expansion(
+        &mut self,
+        spelling_pos: SourcePos,
+        expansion_range: SourceRange,
+        tok_len: u32,
+    ) -> Result<SourceId, SourcesTooLargeError> {
+        self.add_source(
+            || SourceInfo::Expansion(ExpansionSourceInfo::new(spelling_pos, expansion_range)),
+            tok_len,
+        )
+    }
 }
--- source-manager/src/source_pos.rs
+++ source-manager/src/source_pos.rs
@@ -4,79 +4,79 @@ use std::path::Path;
 pub struct SourcePos(u32);
 
 impl SourcePos {
-  pub fn from_raw(raw: u32) -> Self {
-    SourcePos(raw)
-  }
+    pub fn from_raw(raw: u32) -> Self {
+        SourcePos(raw)
+    }
 
-  pub fn to_raw(&self) -> u32 {
-    self.0
-  }
+    pub fn to_raw(&self) -> u32 {
+        self.0
+    }
 
-  pub fn with_offset(&self, offset: i32) -> Self {
-    SourcePos(self.0 + offset as u32)
-  }
+    pub fn with_offset(&self, offset: i32) -> Self {
+        SourcePos(self.0 + offset as u32)
+    }
 }
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 pub struct SourceRange(SourcePos, SourcePos);
 
 impl SourceRange {
-  pub fn new(begin: SourcePos, end: SourcePos) -> Self {
-    SourceRange(begin, end)
-  }
+    pub fn new(begin: SourcePos, end: SourcePos) -> Self {
+        SourceRange(begin, end)
+    }
 
-  pub fn begin(&self) -> SourcePos {
-    self.0
-  }
+    pub fn begin(&self) -> SourcePos {
+        self.0
+    }
 
-  pub fn end(&self) -> SourcePos {
-    self.1
-  }
+    pub fn end(&self) -> SourcePos {
+        self.1
+    }
 }
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 pub struct LineCol {
-  pub line: u32,
-  pub col: u32,
+    pub line: u32,
+    pub col: u32,
 }
 
 #[derive(Clone, Copy)]
 pub struct InterpretedSourcePos<'f> {
-  filename: &'f Path,
-  pos: LineCol,
+    filename: &'f Path,
+    pos: LineCol,
 }
 
 impl<'f> InterpretedSourcePos<'f> {
-  pub fn filename(&self) -> &'f Path {
-    self.filename
-  }
+    pub fn filename(&self) -> &'f Path {
+        self.filename
+    }
 
-  pub fn line(&self) -> u32 {
-    self.pos.line
-  }
+    pub fn line(&self) -> u32 {
+        self.pos.line
+    }
 
-  pub fn col(&self) -> u32 {
-    self.pos.col
-  }
+    pub fn col(&self) -> u32 {
+        self.pos.col
+    }
 }
 
 #[derive(Clone, Copy)]
 pub struct InterpretedSourceRange<'f> {
-  filename: &'f Path,
-  begin: LineCol,
-  end: LineCol,
+    filename: &'f Path,
+    begin: LineCol,
+    end: LineCol,
 }
 
 impl<'f> InterpretedSourceRange<'f> {
-  pub fn filename(&self) -> &'f Path {
-    self.filename
-  }
+    pub fn filename(&self) -> &'f Path {
+        self.filename
+    }
 
-  pub fn begin(&self) -> LineCol {
-    self.begin
-  }
+    pub fn begin(&self) -> LineCol {
+        self.begin
+    }
 
-  pub fn end(&self) -> LineCol {
-    self.end
-  }
+    pub fn end(&self) -> LineCol {
+        self.end
+    }
 }
