aecc75dede8a909220edfda027a3e09eda9d4a34
Noam Raz <noamraz8@gmail.com>
2020-02-08 14:45:19 +0200

    Mark pos methods as inline

--- source-manager/src/pos.rs
+++ source-manager/src/pos.rs
@@ -2,18 +2,22 @@
 pub struct SourcePos(u32);
 
 impl SourcePos {
+    #[inline]
     pub(crate) fn from_raw(raw: u32) -> Self {
         SourcePos(raw)
     }
 
+    #[inline]
     pub(crate) fn to_raw(&self) -> u32 {
         self.0
     }
 
+    #[inline]
     pub fn offset(&self, offset: u32) -> Self {
         SourcePos(self.0 + offset)
     }
 
+    #[inline]
     pub fn offset_from(&self, rhs: SourcePos) -> u32 {
         self.to_raw() - rhs.to_raw()
     }
@@ -23,37 +27,45 @@ impl SourcePos {
 pub struct SourceRange(SourcePos, u32);
 
 impl SourceRange {
+    #[inline]
     pub fn new(begin: SourcePos, len: u32) -> Self {
         SourceRange(begin, len)
     }
 
+    #[inline]
     pub fn start(&self) -> SourcePos {
         self.0
     }
 
+    #[inline]
     pub fn len(&self) -> u32 {
         self.1
     }
 
+    #[inline]
     pub fn end(&self) -> SourcePos {
         self.start().offset(self.len())
     }
 
+    #[inline]
     pub fn subpos(&self, off: u32) -> SourcePos {
         assert!(off < self.len());
         self.start().offset(off)
     }
 
+    #[inline]
     pub fn subrange(&self, off: u32, len: u32) -> SourceRange {
         assert!(off + len <= self.len());
         SourceRange::new(self.start().offset(off), len)
     }
 
+    #[inline]
     pub fn contains(&self, pos: SourcePos) -> bool {
         let raw = pos.to_raw();
         self.start().to_raw() <= raw && raw < self.end().to_raw()
     }
 
+    #[inline]
     pub fn contains_range(&self, other: SourceRange) -> bool {
         self.start().to_raw() <= other.start().to_raw()
             && other.end().to_raw() <= self.end().to_raw()
