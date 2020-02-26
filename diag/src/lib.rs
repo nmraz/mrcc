377ab7d1ee06fb6b4ba6fbc1f83ad7adb73289d8
Noam Raz <noamraz8@gmail.com>
2020-02-26 19:25:01 +0200

    Create basic diagnostic types

--- diag/src/lib.rs
+++ diag/src/lib.rs
@@ -1,7 +1,50 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use source_map::pos::{FragmentedSourceRange, SourceRange};
+
+#[derive(Debug, Clone)]
+pub struct LabeledRange<R>(R, String);
+
+#[derive(Debug, Clone)]
+pub struct Ranges<R> {
+    pub primary_range: R,
+    pub subranges: Vec<LabeledRange<R>>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Level {
+    Note,
+    Warning,
+    Error,
+}
+
+#[derive(Debug, Clone)]
+pub struct SubDiagnostic<R> {
+    pub msg: String,
+    pub ranges: Option<Ranges<R>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Diagnostic<D> {
+    pub level: Level,
+    pub main: D,
+    pub notes: Vec<D>,
+}
+
+pub type RawDiagnostic = Diagnostic<SubDiagnostic<FragmentedSourceRange>>;
+
+#[derive(Debug, Clone)]
+pub struct RenderedSubDiagnostic {
+    pub inner: SubDiagnostic<SourceRange>,
+    pub expansions: Vec<SubDiagnostic<SourceRange>>,
+}
+
+impl RenderedSubDiagnostic {
+    pub fn msg(&self) -> &str {
+        &self.inner.msg
+    }
+
+    pub fn ranges(&self) -> Option<&Ranges<SourceRange>> {
+        self.inner.ranges.as_ref()
     }
 }
+
+pub type RenderedDiagnostic = Diagnostic<RenderedSubDiagnostic>;
