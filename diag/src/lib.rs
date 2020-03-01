9e20218821077ecfce435fca02f28538aed1cc7b
Noam Raz <noamraz8@gmail.com>
2020-03-01 13:09:11 +0200

    Add diagnostic utility methods

--- diag/src/lib.rs
+++ diag/src/lib.rs
@@ -38,11 +38,13 @@ pub struct Ranges<R> {
     pub subranges: Vec<LabeledRange<R>>,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum Level {
-    Note,
-    Warning,
-    Error,
+impl<R> Ranges<R> {
+    pub fn new(primary_range: R) -> Self {
+        Self {
+            primary_range,
+            subranges: Vec::new(),
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -50,6 +52,62 @@ pub struct SubDiagnostic<R> {
     pub msg: String,
     pub ranges: Option<Ranges<R>>,
     pub suggestions: Vec<Suggestion<R>>,
+}
+
+impl<R> SubDiagnostic<R> {
+    pub fn new(msg: impl Into<String>, primary_range: R) -> Self {
+        Self {
+            msg: msg.into(),
+            ranges: Some(Ranges::new(primary_range)),
+            suggestions: Vec::new(),
+        }
+    }
+
+    pub fn new_anon(msg: impl Into<String>) -> Self {
+        Self {
+            msg: msg.into(),
+            ranges: None,
+            suggestions: Vec::new(),
+        }
+    }
+
+    pub fn add_labeled_range(&mut self, range: R, label: impl Into<String>) {
+        self.ranges
+            .as_mut()
+            .expect("cannot attach range to rangeless diagnostic")
+            .subranges
+            .push(LabeledRange(range, label.into()));
+    }
+
+    pub fn add_range(&mut self, range: R) {
+        self.add_labeled_range(range, "");
+    }
+
+    pub fn add_suggestion(&mut self, suggestion: Suggestion<R>) {
+        self.suggestions.push(suggestion)
+    }
+
+    pub fn with_labeled_range(mut self, range: R, label: impl Into<String>) -> Self {
+        self.add_labeled_range(range, label);
+        self
+    }
+
+    pub fn with_range(mut self, range: R) -> Self {
+        self.add_range(range);
+        self
+    }
+
+    pub fn with_suggestion(mut self, suggestion: Suggestion<R>) -> Self {
+        self.add_suggestion(suggestion);
+        self
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Level {
+    Note,
+    Warning,
+    Error,
 }
 
 #[derive(Debug, Clone)]
