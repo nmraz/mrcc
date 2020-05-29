e278e9e51ecd23cff743e678b42491aa728f180a
Noam Raz <noamraz8@gmail.com>
2020-05-30 01:14:07 +0300

    Pull token kinds into separate module

--- lex/src/lib.rs
+++ lex/src/lib.rs
@@ -6,152 +6,15 @@ use smap::pos::{FragmentedSourceRange, SourceRange};
 use smap::SourceMap;
 
 pub mod raw;
+mod token_kind;
+
+pub use token_kind::{CommentKind, PunctKind, TokenKind};
 
 pub type IdentInterner = Interner<str>;
 pub type IdentSym = Symbol<str>;
 
 pub type TokInterner = Interner<str>;
 pub type TokSym = Symbol<str>;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum CommentKind {
-    Line,
-    Block,
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum PunctKind {
-    Hash,
-    HashHash,
-
-    Comma,
-    Colon,
-    Semi,
-
-    LSquare,
-    RSquare,
-    LParen,
-    RParen,
-    LCurly,
-    RCurly,
-
-    Dot,
-    Ellipsis,
-    Arrow,
-
-    Plus,
-    PlusPlus,
-    Minus,
-    MinusMinus,
-    Star,
-    Slash,
-    Perc,
-    Amp,
-    AmpAmp,
-    Pipe,
-    PipePipe,
-    Caret,
-    Tilde,
-    Excl,
-    Question,
-    Less,
-    LessLess,
-    LessEq,
-    Greater,
-    GreaterGreater,
-    GreaterEq,
-
-    Eq,
-    EqEq,
-    ExclEq,
-    PlusEq,
-    MinusEq,
-    StarEq,
-    SlashEq,
-    PercEq,
-    AmpEq,
-    PipeEq,
-    CaretEq,
-    LessLessEq,
-    GreaterGreaterEq,
-}
-
-impl PunctKind {
-    pub fn as_str(self) -> &'static str {
-        use PunctKind::*;
-
-        match self {
-            Hash => "#",
-            HashHash => "##",
-            Comma => ",",
-            Colon => ":",
-            Semi => ";",
-            LSquare => "[",
-            RSquare => "]",
-            LParen => "(",
-            RParen => ")",
-            LCurly => "{",
-            RCurly => "}",
-            Dot => ".",
-            Ellipsis => "...",
-            Arrow => "->",
-            Plus => "+",
-            PlusPlus => "++",
-            Minus => "-",
-            MinusMinus => "--",
-            Star => "*",
-            Slash => "/",
-            Perc => "%",
-            Amp => "&",
-            AmpAmp => "&&",
-            Pipe => "|",
-            PipePipe => "||",
-            Caret => "^",
-            Tilde => "~",
-            Excl => "!",
-            ExclEq => "!=",
-            Question => "?",
-            Less => "<",
-            LessLess => "<<",
-            LessEq => "<=",
-            Greater => ">",
-            GreaterGreater => ">>",
-            GreaterEq => ">=",
-            Eq => "=",
-            EqEq => "==",
-            PlusEq => "+=",
-            MinusEq => "-=",
-            StarEq => "*=",
-            SlashEq => "/=",
-            PercEq => "%=",
-            AmpEq => "&=",
-            PipeEq => "|=",
-            CaretEq => "^=",
-            LessLessEq => "<<=",
-            GreaterGreaterEq => ">>=",
-        }
-    }
-}
-
-impl fmt::Display for PunctKind {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.as_str())
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum TokenKind {
-    Unknown,
-    Eof,
-    Comment(CommentKind),
-
-    Punct(PunctKind),
-
-    Ident(IdentSym),
-    Number(TokSym),
-    Str(TokSym),
-    Char(TokSym),
-}
 
 #[derive(Debug, Clone, Copy)]
 pub struct Token {
---
+++ lex/src/token_kind.rs
@@ -0,0 +1,143 @@
+use std::fmt;
+
+use crate::{IdentSym, TokSym};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum CommentKind {
+    Line,
+    Block,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum PunctKind {
+    Hash,
+    HashHash,
+
+    Comma,
+    Colon,
+    Semi,
+
+    LSquare,
+    RSquare,
+    LParen,
+    RParen,
+    LCurly,
+    RCurly,
+
+    Dot,
+    Ellipsis,
+    Arrow,
+
+    Plus,
+    PlusPlus,
+    Minus,
+    MinusMinus,
+    Star,
+    Slash,
+    Perc,
+    Amp,
+    AmpAmp,
+    Pipe,
+    PipePipe,
+    Caret,
+    Tilde,
+    Excl,
+    Question,
+    Less,
+    LessLess,
+    LessEq,
+    Greater,
+    GreaterGreater,
+    GreaterEq,
+
+    Eq,
+    EqEq,
+    ExclEq,
+    PlusEq,
+    MinusEq,
+    StarEq,
+    SlashEq,
+    PercEq,
+    AmpEq,
+    PipeEq,
+    CaretEq,
+    LessLessEq,
+    GreaterGreaterEq,
+}
+
+impl PunctKind {
+    pub fn as_str(self) -> &'static str {
+        use PunctKind::*;
+
+        match self {
+            Hash => "#",
+            HashHash => "##",
+            Comma => ",",
+            Colon => ":",
+            Semi => ";",
+            LSquare => "[",
+            RSquare => "]",
+            LParen => "(",
+            RParen => ")",
+            LCurly => "{",
+            RCurly => "}",
+            Dot => ".",
+            Ellipsis => "...",
+            Arrow => "->",
+            Plus => "+",
+            PlusPlus => "++",
+            Minus => "-",
+            MinusMinus => "--",
+            Star => "*",
+            Slash => "/",
+            Perc => "%",
+            Amp => "&",
+            AmpAmp => "&&",
+            Pipe => "|",
+            PipePipe => "||",
+            Caret => "^",
+            Tilde => "~",
+            Excl => "!",
+            ExclEq => "!=",
+            Question => "?",
+            Less => "<",
+            LessLess => "<<",
+            LessEq => "<=",
+            Greater => ">",
+            GreaterGreater => ">>",
+            GreaterEq => ">=",
+            Eq => "=",
+            EqEq => "==",
+            PlusEq => "+=",
+            MinusEq => "-=",
+            StarEq => "*=",
+            SlashEq => "/=",
+            PercEq => "%=",
+            AmpEq => "&=",
+            PipeEq => "|=",
+            CaretEq => "^=",
+            LessLessEq => "<<=",
+            GreaterGreaterEq => ">>=",
+        }
+    }
+}
+
+impl fmt::Display for PunctKind {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.as_str())
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum TokenKind {
+    Unknown,
+    Eof,
+    Comment(CommentKind),
+
+    Punct(PunctKind),
+
+    Ident(IdentSym),
+    Number(TokSym),
+    Str(TokSym),
+    Char(TokSym),
+}
