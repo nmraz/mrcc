6c1c37bc8b875eb6ec730b6c5c7b9c466b606889
Noam Raz <noamraz8@gmail.com>
2020-06-18 11:45:38 +0300

    Remove lexer reexports

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,12 +1,12 @@
 pub mod diag;
-mod intern;
 pub mod lex;
-mod pp;
 pub mod smap;
+
+mod intern;
+mod pp;
 mod spos;
 
 pub use diag::Manager as DiagManager;
-pub use lex::{LexCtx, Lexer, Token};
 pub use pp::Preprocessor;
 pub use smap::SourceMap;
 pub use spos::*;
