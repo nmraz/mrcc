0f0c4b71d07cb93df1089a5b75671367fff88fc6
Noam Raz <noamraz8@gmail.com>
2020-06-22 22:41:45 +0300

    Further refactor preprocessor file management

--- frontend/src/pp/file.rs
+++ frontend/src/pp/file.rs
@@ -1,8 +1,8 @@
 use std::rc::Rc;
 
 use crate::lex::raw::Tokenizer;
-use crate::smap::FileContents;
-use crate::SourcePos;
+use crate::smap::{FileContents, SourcesTooLargeError};
+use crate::{SourceMap, SourcePos};
 
 pub struct File {
     contents: Rc<FileContents>,
@@ -27,5 +27,43 @@ impl File {
         self.off += tokenizer.reader.pos() as u32;
 
         ret
+    }
+}
+
+pub struct Files {
+    main: File,
+    includes: Vec<File>,
+}
+
+impl Files {
+    pub fn new(main_contents: Rc<FileContents>, main_start_pos: SourcePos) -> Self {
+        Files {
+            main: File::new(main_contents, main_start_pos),
+            includes: vec![],
+        }
+    }
+
+    pub fn top(&mut self) -> &mut File {
+        self.includes.last_mut().unwrap_or(&mut self.main)
+    }
+
+    pub fn have_includes(&self) -> bool {
+        !self.includes.is_empty()
+    }
+
+    pub fn push_include(
+        &mut self,
+        smap: &mut SourceMap,
+        contents: Rc<FileContents>,
+        include_pos: SourcePos,
+    ) -> Result<(), SourcesTooLargeError> {
+        let id = smap.create_file(Rc::clone(&contents), Some(include_pos))?;
+        self.includes
+            .push(File::new(contents, smap.get_source(id).range.start()));
+        Ok(())
+    }
+
+    pub fn pop_include(&mut self) {
+        self.includes.pop();
     }
 }
--- frontend/src/pp/mod.rs
+++ frontend/src/pp/mod.rs
@@ -4,29 +4,25 @@ use crate::lex::{LexCtx, Lexer, Token};
 use crate::smap::FileContents;
 use crate::SourcePos;
 
-use file::File;
+use file::Files;
 
 mod file;
 
 pub struct Preprocessor {
-    main_file: File,
-    include_stack: Vec<File>,
+    files: Files,
 }
 
 impl Preprocessor {
     pub fn new(contents: Rc<FileContents>, start_pos: SourcePos) -> Self {
         Self {
-            main_file: File::new(contents, start_pos),
-            include_stack: vec![],
+            files: Files::new(contents, start_pos),
         }
     }
 }
 
 impl Lexer for Preprocessor {
     fn next(&mut self, ctx: &mut LexCtx<'_>) -> Token {
-        let file = self.include_stack.last_mut().unwrap_or(&mut self.main_file);
-
-        file.with_tokenizer(|pos, tokenizer| loop {
+        self.files.top().with_tokenizer(|pos, tokenizer| loop {
             let raw = tokenizer.next_token();
             if let Some(tok) = Token::from_raw(&raw, pos, ctx) {
                 break tok;
