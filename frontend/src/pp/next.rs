b767fb61eeff017535033bbb174dbf1f588f7989
Noam Raz <noamraz8@gmail.com>
2020-06-27 01:21:45 +0300

    Create directive handling skeleton

--- frontend/src/pp/next.rs
+++ frontend/src/pp/next.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use crate::lex::{LexCtx, Token};
+use crate::lex::{LexCtx, PunctKind, Token, TokenKind};
 use crate::DResult;
 
 use super::file::FileProcessor;
@@ -12,13 +12,28 @@ pub enum Action {
 }
 
 pub fn next_action(ctx: &mut LexCtx<'_, '_>, processor: &mut FileProcessor<'_>) -> DResult<Action> {
-    let (tok, is_line_start) = loop {
-        let is_line_start = processor.is_line_start();
-        let raw = processor.next_token_skip_ws();
-        if let Some(tok) = Token::from_raw(&raw, processor.base_pos(), ctx)? {
-            break (tok, is_line_start);
-        }
-    };
+    loop {
+        let (tok, is_line_start) = loop {
+            let is_line_start = processor.is_line_start();
+            let raw = processor.next_token_skip_ws();
+            if let Some(tok) = Token::from_raw(&raw, processor.base_pos(), ctx)? {
+                break (tok, is_line_start);
+            }
+        };
 
-    Ok(Action::Tok(tok))
+        if is_line_start && tok.kind == TokenKind::Punct(PunctKind::Hash) {
+            if let Some(action) = handle_directive(ctx, processor)? {
+                break Ok(action);
+            }
+        } else {
+            break Ok(Action::Tok(tok));
+        }
+    }
+}
+
+fn handle_directive(
+    ctx: &mut LexCtx<'_, '_>,
+    processor: &mut FileProcessor<'_>,
+) -> DResult<Option<Action>> {
+    todo!()
 }
