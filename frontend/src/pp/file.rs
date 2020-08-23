ee8d956e8146b5b31b933d32826843ec505235da
Noam Raz <noamraz8@gmail.com>
2020-08-24 00:04:17 +0300

    Create basic include loader

--- frontend/src/pp/file.rs
+++ frontend/src/pp/file.rs
@@ -1,3 +1,4 @@
+use std::borrow::Borrow;
 use std::collections::hash_map::Entry;
 use std::fs;
 use std::io;
@@ -39,11 +40,8 @@ impl FileCache {
         }
     }
 
-    pub fn load(&mut self, path: impl AsRef<Path>) -> io::Result<Rc<File>> {
-        self.do_load(weakly_normalize(path.as_ref()))
-    }
-
-    fn do_load(&mut self, path: PathBuf) -> io::Result<Rc<File>> {
+    pub fn load(&mut self, path: &Path) -> io::Result<Rc<File>> {
+        let path = weakly_normalize(path);
         match self.files.entry(path) {
             Entry::Occupied(ent) => Ok(ent.get().clone()),
             Entry::Vacant(ent) => {
@@ -63,4 +61,70 @@ fn weakly_normalize(path: &Path) -> PathBuf {
     path.components()
         .filter(|&c| c != Component::CurDir)
         .collect()
+}
+
+pub enum IncludeError {
+    NotFound,
+    Io {
+        full_path: PathBuf,
+        error: io::Error,
+    },
+}
+
+pub struct IncludeLoader {
+    cache: FileCache,
+    include_paths: Vec<PathBuf>,
+}
+
+impl IncludeLoader {
+    pub fn new(include_paths: Vec<PathBuf>) -> Self {
+        Self {
+            cache: FileCache::new(),
+            include_paths,
+        }
+    }
+
+    pub fn load(
+        &mut self,
+        path: &Path,
+        kind: IncludeKind,
+        includer: &File,
+    ) -> Result<Rc<File>, IncludeError> {
+        fn do_load(
+            cache: &mut FileCache,
+            full_path: impl Borrow<Path> + Into<PathBuf>,
+        ) -> Result<Rc<File>, IncludeError> {
+            cache.load(full_path.borrow()).map_err(|e| {
+                if e.kind() == io::ErrorKind::NotFound {
+                    IncludeError::NotFound
+                } else {
+                    IncludeError::Io {
+                        full_path: full_path.into(),
+                        error: e,
+                    }
+                }
+            })
+        };
+
+        if path.is_absolute() {
+            return do_load(&mut self.cache, path);
+        }
+
+        let initial_dir = if kind == IncludeKind::Str {
+            includer.parent_dir.as_ref()
+        } else {
+            None
+        };
+
+        let dirs = initial_dir.into_iter().chain(self.include_paths.iter());
+
+        for dir in dirs {
+            match do_load(&mut self.cache, dir.join(path)) {
+                Err(IncludeError::NotFound) => continue,
+                ret => return ret,
+            }
+        }
+
+        Err(IncludeError::NotFound)
+    }
 }
