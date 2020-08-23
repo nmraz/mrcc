7a52f9919e4a255f3a1882ca4a431e6cc1ae6f17
Noam Raz <noamraz8@gmail.com>
2020-08-24 00:42:25 +0300

    Implement basic include handling

--- frontend/src/pp/mod.rs
+++ frontend/src/pp/mod.rs
@@ -1,10 +1,12 @@
 use std::path::PathBuf;
 
+use crate::diag::{FatalErrorEmitted, Level};
 use crate::lex::{LexCtx, Lexer, Token, TokenKind};
-use crate::smap::SourceId;
+use crate::smap::{FileName, SourceId};
 use crate::DResult;
 
 use active_file::{Action, ActiveFiles};
+use file::{IncludeError, IncludeLoader};
 use state::State;
 
 pub use lexer::PpToken;
@@ -16,6 +18,7 @@ mod state;
 
 pub struct Preprocessor {
     active_files: ActiveFiles,
+    include_loader: IncludeLoader,
     state: State,
 }
 
@@ -23,6 +26,7 @@ impl Preprocessor {
     pub fn new(ctx: &mut LexCtx<'_, '_>, main_id: SourceId, parent_dir: Option<PathBuf>) -> Self {
         Self {
             active_files: ActiveFiles::new(&ctx.smap, main_id, parent_dir),
+            include_loader: IncludeLoader::new(Vec::new()),
             state: State::new(ctx),
         }
     }
@@ -37,11 +41,45 @@ impl Preprocessor {
                         break ppt;
                     }
                 }
+
                 Action::Include {
                     filename,
                     kind,
                     pos,
-                } => {}
+                } => {
+                    let file = match self.include_loader.load(
+                        &filename,
+                        kind,
+                        self.active_files.top().file(),
+                    ) {
+                        Ok(file) => file,
+                        Err(err) => {
+                            let msg = match err {
+                                IncludeError::NotFound => {
+                                    format!("include '{}' not found", filename.display())
+                                }
+                                IncludeError::Io { full_path, error } => format!(
+                                    "failed to include '{}' ({}): {}",
+                                    filename.display(),
+                                    full_path.display(),
+                                    error
+                                ),
+                            };
+                            ctx.reporter().report(Level::Fatal, pos, msg).emit()?;
+                            unreachable!();
+                        }
+                    };
+
+                    if self
+                        .active_files
+                        .push_include(&mut ctx.smap, FileName::real(filename), file, pos)
+                        .is_err()
+                    {
+                        ctx.reporter()
+                            .report(Level::Fatal, pos, "translation unit too large")
+                            .emit()?;
+                    }
+                }
             }
         };
 
