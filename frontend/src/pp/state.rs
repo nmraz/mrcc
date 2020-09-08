f9cd96bc29251e02c958e0159f2eda44cf7d2f94
Noam Raz <noamraz8@gmail.com>
2020-09-08 19:46:56 +0300

    Create macro table and replacement lists

--- frontend/src/pp/state.rs
+++ frontend/src/pp/state.rs
@@ -1,13 +1,20 @@
+use rustc_hash::FxHashMap;
+
 use crate::lex::{Interner, LexCtx, Symbol};
+use crate::SourceRange;
+
+use super::PpToken;
 
 pub struct State {
     pub known_idents: KnownIdents,
+    pub macro_table: FxHashMap<Symbol, MacroDef>,
 }
 
 impl State {
     pub fn new(ctx: &mut LexCtx<'_, '_>) -> Self {
         Self {
             known_idents: KnownIdents::new(&mut ctx.interner),
+            macro_table: FxHashMap::default(),
         }
     }
 }
@@ -40,4 +47,45 @@ impl KnownIdents {
             dir_error: interner.intern("error"),
         }
     }
+}
+
+pub struct ReplacementList {
+    tokens: Vec<PpToken>,
+}
+
+impl ReplacementList {
+    pub fn new(mut tokens: Vec<PpToken>) -> Self {
+        if let Some(first) = tokens.first_mut() {
+            first.leading_trivia = false;
+        }
+
+        Self { tokens }
+    }
+
+    pub fn tokens(&self) -> &[PpToken] {
+        &self.tokens
+    }
+
+    pub fn is_identical_to(&self, rhs: &ReplacementList) -> bool {
+        let translate = |ppt: &PpToken| (ppt.kind(), ppt.leading_trivia);
+
+        self.tokens
+            .iter()
+            .map(translate)
+            .eq(rhs.tokens.iter().map(translate))
+    }
+}
+
+pub enum MacroInfo {
+    Object(ReplacementList),
+    Function {
+        args: Vec<Symbol>,
+        replacement: ReplacementList,
+    },
+}
+
+pub struct MacroDef {
+    pub name: Symbol,
+    pub name_range: SourceRange,
+    pub info: MacroInfo,
 }
