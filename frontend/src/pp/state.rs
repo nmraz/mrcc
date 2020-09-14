03b9e6300442fd5f2bf63791fba929b376f7109c
Noam Raz <noamraz8@gmail.com>
2020-09-14 15:37:14 +0300

    Change implementation of `ReplacementList`

--- frontend/src/pp/state.rs
+++ frontend/src/pp/state.rs
@@ -3,7 +3,7 @@ use std::mem;
 
 use rustc_hash::FxHashMap;
 
-use crate::lex::{Interner, LexCtx, Symbol, Token};
+use crate::lex::{Interner, LexCtx, Symbol, Token, TokenKind};
 
 use super::PpToken;
 
@@ -53,29 +53,60 @@ impl KnownIdents {
 
 #[derive(Debug, Clone)]
 pub struct ReplacementList {
-    tokens: Vec<PpToken>,
+    tokens: Vec<MacroToken>,
 }
 
 impl ReplacementList {
-    pub fn new(mut tokens: Vec<PpToken>) -> Self {
+    pub fn new(tokens: impl IntoIterator<Item = PpToken>) -> Self {
+        let mut tokens: Vec<MacroToken> = tokens.into_iter().map(|ppt| ppt.into()).collect();
         if let Some(first) = tokens.first_mut() {
-            first.leading_trivia = false;
+            first.data.leading_trivia = false;
         }
 
         Self { tokens }
     }
 
-    pub fn tokens(&self) -> &[PpToken] {
-        &self.tokens
+    pub fn tokens(&self) -> impl Iterator<Item = PpToken> + '_ {
+        self.tokens.iter().copied().map(|tok| tok.into())
     }
 
     pub fn is_identical_to(&self, rhs: &ReplacementList) -> bool {
-        let translate = |ppt: &PpToken| (ppt.data(), ppt.leading_trivia);
-
         self.tokens
             .iter()
-            .map(translate)
-            .eq(rhs.tokens.iter().map(translate))
+            .map(|tok| tok.data)
+            .eq(rhs.tokens.iter().map(|tok| tok.data))
+    }
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+struct MacroTokenData {
+    kind: TokenKind,
+    leading_trivia: bool,
+}
+
+type MacroToken = Token<MacroTokenData>;
+
+impl From<PpToken> for MacroToken {
+    fn from(ppt: PpToken) -> Self {
+        Self {
+            data: MacroTokenData {
+                kind: ppt.data(),
+                leading_trivia: ppt.leading_trivia,
+            },
+            range: ppt.range(),
+        }
+    }
+}
+
+impl From<MacroToken> for PpToken {
+    fn from(macro_tok: MacroToken) -> Self {
+        let leading_trivia = macro_tok.data.leading_trivia;
+
+        PpToken {
+            tok: macro_tok.map(|data| data.kind),
+            line_start: false,
+            leading_trivia,
+        }
     }
 }
 
