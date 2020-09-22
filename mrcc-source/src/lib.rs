b3ea6f1658ae1919be15001eb19aec3bd0b980d6
Noam Raz <noamraz8@gmail.com>
2020-09-22 14:53:57 +0300

    Begin documenting mrcc-source

--- mrcc-source/src/lib.rs
+++ mrcc-source/src/lib.rs
@@ -1,5 +1,11 @@
 #![warn(rust_2018_idioms)]
 
+//! A library for managing source files, locations and diagnostics.
+//!
+//! The primary exports of this crate are [`SourceMap`](smap/struct.SourceMap.html), which can be
+//! used to track source files and macro expansions, and [`DiagManager`](diag/struct.Manager.html),
+//! for emitting detailed diagnostics with location information.
+
 pub mod diag;
 pub mod smap;
 
--- mrcc-source/src/pos.rs
+++ mrcc-source/src/pos.rs
@@ -1,3 +1,8 @@
+/// An opaque type representing a position in the source code managed by a
+/// [`SourceMap`](../smap/struct.SourceMap.html).
+///
+/// This can be resolved back to file/line/column/expansion information using the appropriate
+/// methods on `SourceMap`.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct SourcePos(u32);
 
@@ -12,69 +17,106 @@ impl SourcePos {
         self.0
     }
 
+    /// Returns a new position lying `offset` characters forward from `self`.
+    ///
+    /// The position returned can be meaningless if the source containing `self` does not contain
+    /// at least `offset` more characters (including a sentinel past-the-end "character").
     #[inline]
     pub fn offset(self, offset: u32) -> Self {
         SourcePos(self.0 + offset)
     }
 
+    /// Returns the distance in characters between `self` and `rhs`, assuming that `rhs` lies before
+    /// `self` in the same source.
     #[inline]
     pub fn offset_from(self, rhs: SourcePos) -> u32 {
         self.to_raw() - rhs.to_raw()
     }
 }
 
+/// Represents a contiguous character range within a single source.
+///
+/// Contrast with [`FragmentedSourceRange`](struct.FragmentedSourceRange.html), which can represent
+/// ranges whose endpoints lie within different sources (such as macro expansions). Generally,
+/// `SourceRange` is preferred when referring to an atomic run of text (i.e. a single token), while
+/// `FragmentedSourceRange` is more flexible and can represent composite structures whose tokens
+/// come from different sources.
+///
+/// `SourceRange` is also useful when displaying diagnostics, where one wants to indicate actual
+/// ranges in the source code.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct SourceRange(SourcePos, u32);
 
 impl SourceRange {
+    /// Creates a new range starting at `begin` and covering `len` characters.
     #[inline]
     pub fn new(begin: SourcePos, len: u32) -> Self {
         SourceRange(begin, len)
     }
 
+    /// Returns the start position of the range.
     #[inline]
     pub fn start(self) -> SourcePos {
         self.0
     }
 
+    /// Returns the length of the range.
     #[inline]
     pub fn len(self) -> u32 {
         self.1
     }
 
+    /// Returns `true` if `self` covers 0 characters.
     #[inline]
     pub fn is_empty(self) -> bool {
         self.1 == 0
     }
 
+    /// Returns a position just past the end of the range.
+    ///
+    /// The range is empty iff `end() == start()`.
     #[inline]
     pub fn end(self) -> SourcePos {
         self.start().offset(self.len())
     }
 
+    /// Returns a position `off` (zero-based) characters into the range.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the range would not contain the returned position.
     #[inline]
     pub fn subpos(self, off: u32) -> SourcePos {
         assert!(off < self.len());
         self.start().offset(off)
     }
 
+    /// Returns a subrange starting `off` (zero-based) characters in and having length `len`.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the range would not contain the returned subrange.
     #[inline]
     pub fn subrange(self, off: u32, len: u32) -> SourceRange {
         assert!(off + len <= self.len());
         SourceRange::new(self.start().offset(off), len)
     }
 
+    /// Checks whether `self` contains `pos`, that is `pos` lies in `[self.start(), self.end())`.
     #[inline]
     pub fn contains(self, pos: SourcePos) -> bool {
         self.start() <= pos && pos < self.end()
     }
 
+    /// Checks whether `self` contains `other`, that is `[other.start(), other.end())` is a subset
+    /// of `[self.start(), self.end())`.
     #[inline]
     pub fn contains_range(self, other: SourceRange) -> bool {
         self.start() <= other.start() && other.end() <= self.end()
     }
 }
 
+/// Converts a position to an empty range around it.
 impl From<SourcePos> for SourceRange {
     #[inline]
     fn from(pos: SourcePos) -> Self {
@@ -82,19 +124,40 @@ impl From<SourcePos> for SourceRange {
     }
 }
 
+/// Represents a range whose endpoints may lie in different sources.
+///
+/// For example, consider the following code:
+///
+/// ```c
+/// #define A 2
+/// int x = A + 1;
+/// ```
+///
+/// No [`SourceRange`](struct.SourceRange.html) can accurately cover the expression `A + 1`, as its
+/// left endpoint lies within the expansion of `A` while its right endpoint lies within the
+/// surrounding file. For this reason, `FragmentedSourceRange` is a better fit for representing
+/// multi-token structures which may contain arbitrarily complex macro expansions.
+///
+/// [`SourceMap::get_unfragmented_range()`](smap/struct.SourceMap.html#method.get_unfragmented_range)
+/// can be used to try to convert a fragmented range to a contiguous range covering both its
+/// endpoints.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct FragmentedSourceRange {
+    /// The starting position of the range.
     pub start: SourcePos,
+    /// A position past the end of the range.
     pub end: SourcePos,
 }
 
 impl FragmentedSourceRange {
+    /// Creates a new range with the specified endpoints.
     #[inline]
     pub fn new(start: SourcePos, end: SourcePos) -> Self {
         FragmentedSourceRange { start, end }
     }
 }
 
+/// Converts a position to a degenerate fragmented range around it.
 impl From<SourcePos> for FragmentedSourceRange {
     #[inline]
     fn from(pos: SourcePos) -> Self {
@@ -102,6 +165,7 @@ impl From<SourcePos> for FragmentedSourceRange {
     }
 }
 
+/// Converts a `SourceRange` to a fragmented range with the same endpoints.
 impl From<SourceRange> for FragmentedSourceRange {
     #[inline]
     fn from(range: SourceRange) -> Self {
@@ -109,9 +173,12 @@ impl From<SourceRange> for FragmentedSourceRange {
     }
 }
 
+/// Represents a simple line-column number pair.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct LineCol {
+    /// Zero-based line number.
     pub line: u32,
+    /// Zero-based column number.
     pub col: u32,
 }
 
