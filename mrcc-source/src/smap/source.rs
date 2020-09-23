5ecc402199e556453092f2a14d1ae2418c1c5f25
Noam Raz <noamraz8@gmail.com>
2020-09-23 23:09:54 +0300

    Document file sources

--- mrcc-source/src/smap/source.rs
+++ mrcc-source/src/smap/source.rs
@@ -11,6 +11,9 @@ mod line_table;
 #[cfg(test)]
 mod tests;
 
+/// Represents a file name, which can either be a real path or a name synthesized by the compiler.
+///
+/// Synthesized names are used for the source code created by a token paste, for example.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum FileName {
     Real(PathBuf),
@@ -18,14 +21,17 @@ pub enum FileName {
 }
 
 impl FileName {
+    /// Creates a new real file name with the specified path.
     pub fn real(path: impl Into<PathBuf>) -> Self {
         FileName::Real(path.into())
     }
 
+    /// Creates a new synthesized file name.
     pub fn synth(name: impl Into<String>) -> Self {
         FileName::Synth(name.into())
     }
 
+    /// Returns `true` if the file name is real.
     pub fn is_real(&self) -> bool {
         match self {
             FileName::Real(_) => true,
@@ -43,12 +49,18 @@ impl fmt::Display for FileName {
     }
 }
 
+/// Represents the contents of a loaded source file.
 pub struct FileContents {
+    /// The source code in the file.
     pub src: String,
+    /// A table used to look up line numbers by file offset.
     line_table: LineTable,
 }
 
 impl FileContents {
+    /// Creates a new `FileContents` with the specified source.
+    ///
+    /// Line endings in the source are normalized.
     pub fn new(src: &str) -> Rc<Self> {
         let normalized_src = src.replace("\r\n", "\n");
         let line_table = LineTable::new_for_src(&normalized_src);
@@ -59,23 +71,44 @@ impl FileContents {
         })
     }
 
+    /// Retrieves the specified portion of the source code.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the range does not lie within the source.
     pub fn get_snippet(&self, range: Range<u32>) -> &str {
         &self.src[range.start as usize..range.end as usize]
     }
 
+    /// Returns the number of lines in the source.
     pub fn line_count(&self) -> u32 {
         self.line_table.line_count()
     }
 
+    /// Computes the line and column numbers for the specified position.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the offset is longer than the source.
     pub fn get_linecol(&self, off: u32) -> LineCol {
         assert!((off as usize) <= self.src.len());
         self.line_table.get_linecol(off)
     }
 
+    /// Obtains the starting offset within the source of the specified (zero-based) line number.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the line number is out of range.
     pub fn get_line_start(&self, line: u32) -> u32 {
         self.line_table.get_line_start(line)
     }
 
+    /// Obtains the ending offset within the source of the specified (zero-based) line number.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the line number is out of range.
     pub fn get_line_end(&self, line: u32) -> u32 {
         assert!(line < self.line_count());
 
@@ -86,25 +119,43 @@ impl FileContents {
         }
     }
 
+    /// Returns a reference to lines `first..=last` of the source code, including final newline (if
+    /// present).
+    ///
+    /// # Panics
+    ///
+    /// Panics if either line number is out of range or if `first > last`.
     pub fn get_lines(&self, first: u32, last: u32) -> &str {
         let start = self.get_line_start(first);
         let end = self.get_line_end(last);
         self.get_snippet(start..end)
     }
 
+    /// Returns a reference to the specified line of source code, including newline character (if
+    /// present).
+    ///
+    /// # Panics
+    ///
+    /// Panics if the line number is out of range.
     pub fn get_line(&self, line: u32) -> &str {
         self.get_lines(line, line)
     }
 }
 
+/// Holds information about a file [source](struct.SourceMap.html#sources).
 #[derive(Clone)]
 pub struct FileSourceInfo {
+    /// The name of the file.
     pub filename: FileName,
+    /// The contents of the file. Multiple file sources may share the same contents (e.g. when the
+    /// same file is included multiple times).
     pub contents: Rc<FileContents>,
+    /// The position at which this file was included, if any.
     pub include_pos: Option<SourcePos>,
 }
 
 impl FileSourceInfo {
+    /// Creates a new `FileSourceInfo`.
     pub fn new(
         filename: FileName,
         contents: Rc<FileContents>,
