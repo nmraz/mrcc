d73fe017db14a0c2bfd5f660caa294773c18b0ee
Noam Raz <noamraz8@gmail.com>
2020-10-17 22:31:59 +0300

    Add trivia token tests

--- mrcc-lex/src/raw/tests.rs
+++ mrcc-lex/src/raw/tests.rs
@@ -15,3 +15,61 @@ fn skip_escaped_newlines() {
     assert!(escaped.tainted());
 }
 
+fn first_token(input: &str) -> RawToken<'_> {
+    Tokenizer::new(input).next_token()
+}
+
+fn check_single_token_kind(input: &str, kind: RawTokenKind) {
+    let tok = first_token(input);
+    assert_eq!(tok.kind, kind);
+    assert_eq!(tok.content.str, input);
+}
+
+#[test]
+fn eof() {
+    check_single_token_kind("", RawTokenKind::Eof);
+}
+
+#[test]
+fn newline() {
+    check_single_token_kind("\n", RawTokenKind::Newline);
+}
+
+#[test]
+fn whitespace() {
+    check_single_token_kind(" \x0c \t \x0b", RawTokenKind::Ws);
+
+    let tok = first_token(" \x0c \t \x0b\n ");
+    assert_eq!(tok.kind, RawTokenKind::Ws);
+    assert_eq!(tok.content.str, " \x0c \t \x0b");
+}
+
+#[test]
+fn line_comment() {
+    check_single_token_kind("// comment text", RawTokenKind::LineComment);
+    check_single_token_kind("// comment\\\n text", RawTokenKind::LineComment);
+
+    let tok = first_token("// comment\n text");
+    assert_eq!(tok.kind, RawTokenKind::LineComment);
+    assert_eq!(tok.content.str, "// comment");
+}
+
+#[test]
+fn block_comment() {
+    check_single_token_kind(
+        "/* comment text */",
+        RawTokenKind::BlockComment { terminated: true },
+    );
+    check_single_token_kind(
+        "/* comment /* text */",
+        RawTokenKind::BlockComment { terminated: true },
+    );
+    check_single_token_kind(
+        "/* comment\n text */",
+        RawTokenKind::BlockComment { terminated: true },
+    );
+    check_single_token_kind(
+        "/* comment text",
+        RawTokenKind::BlockComment { terminated: false },
+    );
+}
