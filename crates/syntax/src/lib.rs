63954c307e2eebcdc72b588463641463f9c08da1
Noam Raz <noamraz8@gmail.com>
2021-02-02 12:09:16 +0200

    Sketch out basic `syntax` API

--- crates/syntax/src/lib.rs
+++ crates/syntax/src/lib.rs
@@ -1,5 +1,80 @@
 #![warn(rust_2018_idioms)]
 
+use source::FragmentedSourceRange;
+
 pub use kind::*;
 
 mod kind;
+
+pub type Token = lex::Token<TokenKind>;
+
+#[derive(Debug)]
+pub struct Node {
+    kind: NodeKind,
+    range: FragmentedSourceRange,
+    children: Vec<Element>,
+}
+
+impl Node {
+    pub fn new(kind: NodeKind, children: Vec<Element>) -> Self {
+        let first = children.first().expect("passed empty children");
+        let last = children.last().unwrap();
+
+        let range = FragmentedSourceRange::new(first.range().start, last.range().end);
+
+        Self {
+            kind,
+            range,
+            children,
+        }
+    }
+
+    pub fn kind(&self) -> NodeKind {
+        self.kind
+    }
+
+    pub fn range(&self) -> FragmentedSourceRange {
+        self.range
+    }
+
+    pub fn children(&self) -> impl Iterator<Item = &'_ Element> {
+        self.children.iter()
+    }
+
+    pub fn child_nodes(&self) -> impl Iterator<Item = &'_ Node> {
+        self.children().filter_map(Element::as_node)
+    }
+
+    pub fn child_tokens(&self) -> impl Iterator<Item = &'_ Token> {
+        self.children().filter_map(Element::as_token)
+    }
+}
+
+#[derive(Debug)]
+pub enum Element {
+    Node(Node),
+    Token(Token),
+}
+
+impl Element {
+    pub fn as_node(&self) -> Option<&Node> {
+        match self {
+            Element::Node(node) => Some(node),
+            _ => None,
+        }
+    }
+
+    pub fn as_token(&self) -> Option<&Token> {
+        match self {
+            Element::Token(tok) => Some(tok),
+            _ => None,
+        }
+    }
+
+    pub fn range(&self) -> FragmentedSourceRange {
+        match self {
+            Element::Node(node) => node.range(),
+            Element::Token(tok) => tok.range.into(),
+        }
+    }
+}
