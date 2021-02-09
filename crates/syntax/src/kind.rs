3b1457b6d0d54cb97c38eff4b6fd4f5f6b7ff8ca
Noam Raz <noamraz8@gmail.com>
2021-02-09 18:17:51 +0200

    Add converting constructor for syntax token kinds

--- crates/syntax/src/kind.rs
+++ crates/syntax/src/kind.rs
@@ -1,3 +1,5 @@
+use lex::Interner;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Keyword {
     Alignof,
@@ -50,6 +52,65 @@ pub enum Keyword {
 pub enum TokenKind {
     Plain(lex::TokenKind),
     Keyword(Keyword),
+}
+
+impl TokenKind {
+    pub fn from_plain(plain: lex::TokenKind, interner: &Interner) -> Self {
+        let ident = match plain {
+            lex::TokenKind::Ident(ident) => ident,
+            _ => return Self::Plain(plain),
+        };
+
+        let kw = match &interner[ident] {
+            "alignof" => Keyword::Alignof,
+            "auto" => Keyword::Auto,
+            "break" => Keyword::Break,
+            "case" => Keyword::Case,
+            "char" => Keyword::Char,
+            "const" => Keyword::Const,
+            "continue" => Keyword::Continue,
+            "default" => Keyword::Default,
+            "do" => Keyword::Do,
+            "double" => Keyword::Double,
+            "else" => Keyword::Else,
+            "enum" => Keyword::Enum,
+            "extern" => Keyword::Extern,
+            "float" => Keyword::Float,
+            "for" => Keyword::For,
+            "goto" => Keyword::Goto,
+            "if" => Keyword::If,
+            "inline" => Keyword::Inline,
+            "int" => Keyword::Int,
+            "long" => Keyword::Long,
+            "register" => Keyword::Register,
+            "restrict" => Keyword::Restrict,
+            "return" => Keyword::Return,
+            "short" => Keyword::Short,
+            "signed" => Keyword::Signed,
+            "sizeof" => Keyword::Sizeof,
+            "static" => Keyword::Static,
+            "struct" => Keyword::Struct,
+            "switch" => Keyword::Switch,
+            "typedef" => Keyword::Typedef,
+            "union" => Keyword::Union,
+            "unsigned" => Keyword::Unsigned,
+            "void" => Keyword::Void,
+            "volatile" => Keyword::Volatile,
+            "while" => Keyword::While,
+            "_Alignas" => Keyword::Alignas,
+            "_Atomic" => Keyword::Atomic,
+            "_Bool" => Keyword::Bool,
+            "_Complex" => Keyword::Complex,
+            "_Generic" => Keyword::Generic,
+            "_Imaginary" => Keyword::Imaginary,
+            "_Noreturn" => Keyword::Noreturn,
+            "_Static_assert" => Keyword::StaticAssert,
+            "_Thread_local" => Keyword::ThreadLocal,
+            _ => return Self::Plain(plain),
+        };
+
+        Self::Keyword(kw)
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
